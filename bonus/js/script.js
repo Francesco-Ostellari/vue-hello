4fc6418f74fee4e5a81ff76ea555da8d41a3a150
Francesco-Ostellari <francescoostellari@gmail.com>
2021-12-15 15:54:50 +0100

    add-first-bonus

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,12 +1,19 @@
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 // Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 
+// bonus
+// Inseriamo un input per visualizzare in pagina un testo inserito da un utente.
+// Inseriamo una immagine e accanto un numero che identifica il numero di prodotti desiderati, i dati devono venire da Vue.
+// Poi aggiungiamo un pulsante per aumentare questo numero.
+
 const app = new Vue(
   {
     el: '#container',
     data: {
       message: 'Ciao!',
-      image: 'https://a.travel-assets.com/findyours-php/viewfinder/images/res40/36000/36114.jpg'
+      image: 'https://a.travel-assets.com/findyours-php/viewfinder/images/res40/36000/36114.jpg',
+      text: '',
+      placeHolderMessage: 'Inserisci un messaggio'
     }
   }
 );
